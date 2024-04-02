a18aa7b5d0bea854ec864b05a644042ae9a4213a
GSolivier <guilhermesousa1110@gmail.com>
2024-04-02 10:54:19 -0300

    get appointments att

--- WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/WebAPI/obj/Debug/net7.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34176e19284035deadf88f561659b488c02b5063")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dc6f6fb78248b01aa00b7ace7a296a97c962f55")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
